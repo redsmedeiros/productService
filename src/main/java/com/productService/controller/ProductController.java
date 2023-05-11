47708eeaf6d9638a2b08caa9d5dbef3ef10e55b4
Rodolpho <redsmedeiros@gmail.com>
2023-05-11 19:42:59 -0300

    primeiro

---
+++ src/main/java/com/productService/controller/ProductController.java
@@ -0,0 +1,10 @@
+package com.productService.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/product")
+public class ProductController {
+    
+}
---
+++ src/main/java/com/productService/model/Product.java
@@ -0,0 +1,24 @@
+package com.productService.model;
+
+import java.math.BigDecimal;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Document(value = "product")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Product {
+    @Id
+    private String id;
+    private String name;
+    private String description;
+    private BigDecimal price;
+}
