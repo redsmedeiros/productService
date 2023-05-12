4fdd9b02af1d212a32967637fbfa5ecd9e2e5856
Rodolpho <redsmedeiros@gmail.com>
2023-05-12 15:16:10 -0300

    teste para o post controller

--- src/main/java/com/productService/controller/ProductController.java
+++ src/main/java/com/productService/controller/ProductController.java
@@ -2,6 +2,7 @@ package com.productService.controller;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -18,10 +19,13 @@ import lombok.RequiredArgsConstructor;
 
 @RestController
 @RequestMapping("/api/product")
-@RequiredArgsConstructor
 public class ProductController {
 
     private final ProductService productService;
+
+    public ProductController(ProductService productService){
+        this.productService = productService;
+    }
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
